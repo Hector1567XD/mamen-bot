ab7ecef0992983f14f619cca45ee45cdc925eb89
Feredev <hector1567xd@gmail.com>
2023-02-18 14:37:37 -0400

    Fixes in mamenbot!

--- routes/botman.php
+++ routes/botman.php
@@ -2,25 +2,19 @@
 use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
-
-$frasesDeMamen = [
-    "Huele boxer",
-    "Nadie:",
-    "Mi pana el menos esquizo",
-    "Xd",
-    "si",
-    "En efecto",
-    "Turbio",
-    "Las condiciones en las que nacemos no determinan las personas en las que nos convertiremos",
-    "Chicos, recuerden, lo mas importante es aceptarse a uno mismo, cuidar el autoestima y respetarnos los unos a los otros"
-];
  
 $botman->hears('di lo tuyo mamen!', function ($bot) {
-    $key = array_rand($frasesDeMamen);
-    $bot->reply($frasesDeMamen[$key]);
-});
-
-$botman->hears('Di lo tuyo mamen!', function ($bot) {
+    $frasesDeMamen = [
+        "Huele boxer",
+        "Nadie:",
+        "Mi pana el menos esquizo",
+        "Xd",
+        "si",
+        "En efecto",
+        "Turbio",
+        "Las condiciones en las que nacemos no determinan las personas en las que nos convertiremos",
+        "Chicos, recuerden, lo mas importante es aceptarse a uno mismo, cuidar el autoestima y respetarnos los unos a los otros"
+    ];
     $key = array_rand($frasesDeMamen);
     $bot->reply($frasesDeMamen[$key]);
 });
